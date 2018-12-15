bde8a9f544067a18e1d8a6d092201fa31cfeb194
Andrew Baker <andrew@chronalstudios.com>
2018-12-15 06:04:41 -0500

    Help Menu Update
    Cleaned up the menu & updated it.

--- bot.py
+++ bot.py
@@ -8,11 +8,11 @@ import youtube_dl
 import json
 import os
 
-#Prefix
+# Prefix
 
 bot = commands.Bot(command_prefix='!')
-
 extensions = ['moderation', 'starter', 'verify', 'clean', 'log']
+bot.remove_command('help')
 
 if __name__ == '__main__':
     for extension in extensions:
@@ -23,8 +23,6 @@ if __name__ == '__main__':
 
 players = {}
 queues = {}
-
-bot.remove_command('help')
 
 # Status Cycle
 
@@ -57,7 +55,7 @@ async def on_ready():
     print ("Bot running under user: {}".format(bot.user))
     #await bot.change_presence(game=discord.Game(name='Managing The Server'))
 
-#Commands
+# Basic Commands
 
 @bot.command(pass_context=True)
 async def ping(ctx):
@@ -78,16 +76,33 @@ async def help(ctx):
     embed = discord.Embed(color = 0xfc4156)
     embed.set_author(name='Requesting Help Report', icon_url="https://cdn.discordapp.com/app-icons/481923206848970803/394817ba790d2fbb9c36715a7ec00576.png")
     embed.add_field(name='Introduction', value="For any major problems, please seek out the server staff. For any problems relating to the functioning of the bot, alert the creator of the problem and wait for a fix. The help menu consists of all commands relating to the server. Some of these commands may only be ran by a user with the appropriate rank. To see the commands, please check below.", inline=False)
-    embed.add_field(name='Moderation Commands ──────────────────────────────────────', value="Use these to moderate the server", inline=False)
+    embed.add_field(name='__**Getting Started**__', value="Installing the bot.", inline=False)
+    embed.add_field(name='Install Delta', value="!intro | Installs the bot's dependencies.", inline=False)
+    embed.add_field(name='__**Moderation Commands**__', value="Use these to moderate the server", inline=False)
     embed.add_field(name='Ban User', value="!ban | Bans a user from the server, indefinitely.", inline=False)
     embed.add_field(name='Kick User', value="!kick | Kicks a user from the server.", inline=False)
     embed.add_field(name='Clear Messages', value="!clear (Number) | Clears a number of messages from the server.", inline=False)
-    embed.add_field(name='Information Commands ──────────────────────────────────────', value="Use these to get information.", inline=False)
+    embed.add_field(name='Purge Messages', value="!purge (Number) | Purges a number of messages from the server.", inline=False)
+    embed.add_field(name='Hammer User', value="!hammer | An advanced form of a ban deleting messages.", inline=False)
+    embed.add_field(name='__**Information Commands**__', value="Use these to get information.", inline=False)
     embed.add_field(name='Identify User', value="!Identify (name) | Pulls a user's file from the server.", inline=False)
     embed.add_field(name='Server Information', value="!serverinfo | Shows information about the active server.", inline=False)
-    embed.add_field(name='Miscellaneous Commands ──────────────────────────────────────', value="Added on commands.", inline=False)
+    embed.add_field(name='__**Music Commands**__', value="Music Playback Commands.", inline=False)
+    embed.add_field(name='Play Song', value="!play (Terms) | Plays a song by the artist specified, or grabs the specific song.", inline=False)
+    embed.add_field(name='Pause Song', value="!pause | Pauses the active playback.", inline=False)
+    embed.add_field(name='Queue Song', value="!queue (Terms) | Adds a song into the queue.", inline=False)
+    embed.add_field(name='Stop Song', value="!stop | Stops current playback.", inline=False)
+    embed.add_field(name='Skip Song', value="!skip | Skips current playback.", inline=False)
+    embed.add_field(name='Join Channel', value="!join | Bot joins your voice channel.", inline=False)
+    embed.add_field(name='Leave Channel', value="!leave | Bot leaves your voice channel.", inline=False)
+    embed.add_field(name='__**Verify Commands**__', value="Terms & Conditions.", inline=False)
+    embed.add_field(name='Accept Terms', value="!accept | Accepts Terms of the Server and grants entry.", inline=False)
+    embed.add_field(name='__**Miscellaneous Commands**__', value="Added on commands.", inline=False)
     embed.add_field(name='Echo Command', value="!echo (text) | The bot echoes the text given to it.", inline=False)
+    embed.add_field(name='Ping Command', value="!ping | It replies back with pong.", inline=False)
     await bot.send_message(author, embed=embed)
+
+# Music Commands
 
 @bot.command(pass_context=True)
 async def join(ctx):
@@ -151,11 +166,11 @@ async def skip(ctx):
     else:
         await bot.say("**Queue is Empty**")
 
-#Tasks
+# Tasks
 
 bot.loop.create_task(change_status())
 
-#Tokens
+# Tokens
 
 Token = "NDgxOTIzMjA2ODQ4OTcwODAz.DrpPng.oKeZXc2hieFe4R7_ETK9Q_tewu0"
 bot.run(Token)
